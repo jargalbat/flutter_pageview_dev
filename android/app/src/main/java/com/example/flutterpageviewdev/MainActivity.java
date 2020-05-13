40859e8da062aa607d2de1cb888f13fc6a663739
Jargalbat <jargalbat.j@grapecity.mn>
2020-05-13 20:10:17 +0800

    first commit

---
+++ android/app/src/main/java/com/example/flutterpageviewdev/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutterpageviewdev;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/custom_pageview.dart
@@ -0,0 +1,151 @@
+import 'dart:math';
+
+//https://medium.com/flutter-community/flutter-pageview-widget-e0f6c8092636
+
+import 'package:flutter/material.dart';
+
+class CustomPageView extends StatefulWidget {
+  @override
+  _CustomPageViewState createState() => _CustomPageViewState();
+}
+
+class _CustomPageViewState extends State<CustomPageView> {
+  PageController _controller = PageController(initialPage: 0, viewportFraction: 0.8);
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  static const _kDuration = const Duration(milliseconds: 300);
+  static const _kCurve = Curves.ease;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        /// Page view
+        Container(
+          height: 300.0,
+          child: PageView(
+            controller: _controller,
+            children: [
+              _child1(),
+              _child2(),
+              _child3(),
+            ],
+          ),
+        ),
+
+        /// Indicator
+        DotsIndicator(
+          controller: _controller,
+          itemCount: 3,
+          onPageSelected: (int page) {
+            _controller.animateToPage(
+              page,
+              duration: _kDuration,
+              curve: _kCurve,
+            );
+          },
+          color: Colors.red,
+        ),
+      ],
+    );
+  }
+
+  Widget _child1() {
+    return Container(
+      color: Colors.deepPurple,
+      height: 200.0,
+      width: 200.0,
+      child: Icon(Icons.event),
+    );
+  }
+
+  Widget _child2() {
+    return Container(
+      color: Colors.red,
+      height: 200.0,
+      width: 200.0,
+      child: Icon(Icons.event),
+    );
+  }
+
+  Widget _child3() {
+    return Container(
+      color: Colors.green,
+      height: 200.0,
+      width: 200.0,
+      child: Icon(Icons.event),
+    );
+  }
+}
+
+// An indicator showing the currently selected page of a PageController
+class DotsIndicator extends AnimatedWidget {
+  DotsIndicator({
+    this.controller,
+    this.itemCount,
+    this.onPageSelected,
+    this.color: Colors.white,
+  }) : super(listenable: controller);
+
+  /// The PageController that this DotsIndicator is representing.
+  final PageController controller;
+
+  /// The number of items managed by the PageController
+  final int itemCount;
+
+  /// Called when a dot is tapped
+  final ValueChanged<int> onPageSelected;
+
+  /// The color of the dots.
+  ///
+  /// Defaults to `Colors.white`.
+  final Color color;
+
+  // The base size of the dots
+  static const double _kDotSize = 8.0;
+
+  // The increase in the size of the selected dot
+  static const double _kMaxZoom = 2.0;
+
+  // The distance between the center of each dot
+  static const double _kDotSpacing = 25.0;
+
+  Widget _buildDot(int index) {
+    double selectedness = Curves.easeOut.transform(
+      max(
+        0.0,
+        1.0 - ((controller.page ?? controller.initialPage) - index).abs(),
+      ),
+    );
+    double zoom = 1.0 + (_kMaxZoom - 1.0) * selectedness;
+    return new Container(
+      width: _kDotSpacing,
+      child: new Center(
+        child: new Material(
+          color: color,
+          type: MaterialType.circle,
+          child: new Container(
+            width: _kDotSize * zoom,
+            height: _kDotSize * zoom,
+            child: new InkWell(
+              onTap: () => onPageSelected(index),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: new List<Widget>.generate(itemCount, _buildDot),
+    );
+  }
+}
